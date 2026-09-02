4668fa19f8d21daba572563df02ff951204585a8
Vinidevkz <viniynl710@gmail.com>
2026-09-01 21:05:26 -0300

    first commit on repository

---
+++ src/main/java/com/app/unotes/UnotesApplication.java
@@ -0,0 +1,13 @@
+package com.app.unotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/unotes/UnotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.unotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
