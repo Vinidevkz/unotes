f25354d256c420867a5d2aa93b28f7d0627eee13
Vinidevkz <viniynl710@gmail.com>
2026-09-01 21:48:36 -0300

    chore(Aluno e Grade): adicionando a classe de Aluno e Grade ao sistema.

---
+++ src/main/java/com/app/unotes/entities/Aluno.java
@@ -0,0 +1,35 @@
+package com.app.unotes.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_alunos")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Aluno {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+    private String nome_aluno;
+    private String sobrenome_aluno;
+    @Column(unique = true)
+    private String email_aluno;
+    private String senha_aluno;
+    private String biografia_aluno;
+    private String curso_aluno;
+    private String data_nascimento_aluno;
+
+    @OneToMany
+    @JoinColumn(name = "id_aluno")
+    private List<Grade> grades_aluno = new ArrayList<>();
+
+}
---
+++ src/main/java/com/app/unotes/entities/Grade.java
@@ -0,0 +1,18 @@
+package com.app.unotes.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Table(name = "tb_grades")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Grade {
+
+
+
+}
