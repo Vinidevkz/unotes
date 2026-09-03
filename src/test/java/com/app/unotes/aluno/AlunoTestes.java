e7e9bea75edad4586ded44b3b1d9d54ad21ba616
Vinidevkz <viniynl710@gmail.com>
2026-09-03 19:48:12 -0300

    test(aluno): adiconei a classe para implementação de testes dos alunos.

---
+++ src/test/java/com/app/unotes/aluno/AlunoTestes.java
@@ -0,0 +1,12 @@
+package com.app.unotes.aluno;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AlunoTestes {
+
+
+}
